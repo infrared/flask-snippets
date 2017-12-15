1ea66cc59cba445771b39e55344ef56c4a766f3f
Michael Kroher <michael@kroher.net>
2017-12-15 07:16:16 -0500

    json-wtforms-validation unit tests
    added unit test to verify validation still works with
    non-json data

--- json-wtforms-validation/test_json_wtforms_validation.py
+++ json-wtforms-validation/test_json_wtforms_validation.py
@@ -4,15 +4,34 @@ sys.path.append('.')
 from app import app
 
 
-def test_app():
+def test_app_bad_validation():
     client = app.test_client()
     headers = {'Content-Type': 'application/json'}
     res = client.post('/api/widgets', data=json.dumps(dict()), headers=headers)
-    assert res
+    data = json.loads(res.data)
+    assert data.get('name')[0] == "This field is required."
+
+
+def test_app_bad_validation_name():
+    client = app.test_client()
+    headers = {'Content-Type': 'application/json'}
+    res = client.post('/api/widgets', data=json.dumps(dict(name='bar')),
+                      headers=headers)
+    data = json.loads(res.data)
+    assert data.get('name')[0].startswith('Invalid value')
 
 
 def test_app_good():
     client = app.test_client()
     headers = {'Content-Type': 'application/json'}
-    res = client.post('/api/widgets', data=json.dumps(dict(name='oop')), headers=headers)
-    print res.data
+    res = client.post('/api/widgets', data=json.dumps(dict(name='oop')),
+                      headers=headers)
+    data = json.loads(res.data)
+    assert data.get('message') == "Thanks for the widget"
+
+
+def test_app_good_non_json():
+    client = app.test_client()
+    res = client.post('/api/widgets', data=dict(name='oop'))
+    data = json.loads(res.data)
+    assert data.get('message') == "Thanks for the widget"
